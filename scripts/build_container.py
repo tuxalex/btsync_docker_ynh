b8401463ffb934581bae2f1da75ac110d7110551
tuxalex <alexan.andrieux@tuxcloud.fr>
2015-09-30 21:12:59 +0200

    update python script

--- scripts/build_container.py
+++ scripts/build_container.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
 from docker import Client
 
 name='btsync'
 cli = Client(base_url='unix://var/run/docker.sock')
-[line for line in cli.build(path='../build/', rm=True, tag='test/'+name)]
-cli.create_container(image="test/"+name, detach=True, ports=[8888, 5555], tty=True, volumes=["/home/yunohost.app/owncloud/data/:/data/", "/home/btsync_config/:/opt/btsync"], name=name)
-cli.inspect_container(container=name)
-cli.start(container=name, publish_all_ports=True)
-cli.containers()
+config=cli.create_host_config(port_bindings={
+        			8888: 8888,
+        			5555: 5555
+    	})
+
+for line in cli.build(path='../build/', rm=True, tag='test/'+name):
+	out = json.loads(line)
+	print(out['stream'])
+
+container = cli.create_container(
+			image="test/"+name, 
+			detach=True,  
+			tty=True, 
+			volumes=["/home/yunohost.app/owncloud/data/:/data/", "/home/btsync_config/:/opt/btsync"], 
+			name=name,
+			host_config=config
+)
+
+
+print(container)		
+out = cli.start(container=name) 
+print(out)
+	
 exit()
 
